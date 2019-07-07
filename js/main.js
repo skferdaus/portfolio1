2c7d5764436f252cbd7e3b8e730c4994919d4387
skferdausa@gmail.com <you@example.com>
2019-07-07 09:44:13 +0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,68 @@
+// external js: isotope.pkgd.js
+
+/*// init Isotope
+var $grid = $('.grid').isotope({
+  itemSelector: '.element-item',
+  layoutMode: 'fitRows'
+});
+// filter functions
+var filterFns = {
+  // show if number is greater than 50
+  numberGreaterThan50: function() {
+    var number = $(this).find('.number').text();
+    return parseInt( number, 10 ) > 50;
+  },
+  // show if name ends with -ium
+  ium: function() {
+    var name = $(this).find('.name').text();
+    return name.match( /ium$/ );
+  }
+};
+// bind filter button click
+$('.filters-button-group').on( 'click', 'button', function() {
+  var filterValue = $( this ).attr('data-filter');
+  // use filterFn if matches value
+  filterValue = filterFns[ filterValue ] || filterValue;
+  $grid.isotope({ filter: filterValue });
+});
+// change is-checked class on buttons
+$('.button-group').each( function( i, buttonGroup ) {
+  var $buttonGroup = $( buttonGroup );
+  $buttonGroup.on( 'click', 'button', function() {
+    $buttonGroup.find('.is-checked').removeClass('is-checked');
+    $( this ).addClass('is-checked');
+  });
+});
+*/
+/*All Pluging js*/
+/*js for paralux*/
+$('.parallax-window').parallax({imageSrc: 'img/countdown.jpg'});
+
+/*countup js */
+
+
+/*bootstrap scrollspy js */
+
+$('body').scrollspy({ target: '#navbar-example' });
+$('[data-spy="scroll"]').each(function () {
+  var $spy = $(this).scrollspy('refresh')
+});
+
+/*olow carosal*/
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:5,
+	width:170,
+    nav:true,
+    responsive:{
+        0:{
+            items:2
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
